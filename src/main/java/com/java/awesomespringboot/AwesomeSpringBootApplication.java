98e067287eb032237f7837ba39e995db2f6ab2e4
Dipak <dipakkadamdpk@gmail.com>
2023-03-28 20:14:42 +0530

    Initial commit

---
+++ src/main/java/com/java/awesomespringboot/AwesomeSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.java.awesomespringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwesomeSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwesomeSpringBootApplication.class, args);
+		System.out.println("Hello world!");
+	}
+
+}
---
+++ src/test/java/com/java/awesomespringboot/AwesomeSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.awesomespringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwesomeSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
