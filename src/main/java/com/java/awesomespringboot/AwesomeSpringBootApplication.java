75dbed43e3fc7453f9a328d1b5e5fc0f0f8bd07b
Dipak <dipakkadamdpk@gmail.com>
2023-03-30 07:27:35 +0530

    Testing with @component annotation.

--- src/main/java/com/java/awesomespringboot/AwesomeSpringBootApplication.java
+++ src/main/java/com/java/awesomespringboot/AwesomeSpringBootApplication.java
@@ -1,14 +1,18 @@
 package com.java.awesomespringboot;
 
+import com.java.awesomespringboot.sub1.SpringComponentDemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class AwesomeSpringBootApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AwesomeSpringBootApplication.class, args);
-		System.out.println("Hello world!");
+		ConfigurableApplicationContext applicationContext = SpringApplication.run(AwesomeSpringBootApplication.class, args);
+		SpringComponentDemo componentDemo = applicationContext.getBean(SpringComponentDemo.class);
+		System.out.println(componentDemo.getMessage());
+		System.out.println("Hello world from main!");
 	}
 
 }
---
+++ src/main/java/com/java/awesomespringboot/sub1/SpringComponentDemo.java
@@ -0,0 +1,10 @@
+package com.java.awesomespringboot.sub1;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class SpringComponentDemo {
+    public String getMessage() {
+        return "I am fromSpringComponentDemo";
+    }
+}
